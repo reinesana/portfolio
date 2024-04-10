bfe13f86bf3258b575016043058d464f22d70150
reinesana <153279125+reinesana@users.noreply.github.com>
2024-04-09 17:15:06 -0700

    pushing hero page

--- port_v2/src/components/Hero.js
+++ port_v2/src/components/Hero.js
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Hero.css'; 
 import leftImage from '/Users/shanakesia/Desktop/CMPT/projects/portfolio/port_v2/src/components/images/left_image.png';
-import rightImage from '/Users/shanakesia/Desktop/CMPT/projects/portfolio/port_v2/src/components/images/right_image.png'
+import rightImage from '/Users/shanakesia/Desktop/CMPT/projects/portfolio/port_v2/src/components/images/right_image.png';
+
 function Hero() {
-        
+  const headingRef = useRef(null); // Create a ref for the animated text
+
+  useEffect(() => {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let interval = null;
+
+    const animateText = (event) => {
+      let iteration = 0;
+
+      clearInterval(interval);
+
+      interval = setInterval(() => {
+        const targetText = event.target.getAttribute('data-value') || "";
+        event.target.innerText = targetText
+          .split("")
+          .map((char, index) => {
+            if (index < iteration) {
+              return targetText[index];
+            }
+            return letters[Math.floor(Math.random() * 26)];
+          })
+          .join("");
+
+        if (iteration >= targetText.length) {
+          clearInterval(interval);
+        }
+
+        iteration += 1 / 3;
+      }, 20);
+    };
+
+    // Attach the event listener
+    const heading = headingRef.current;
+    if (heading) {
+      heading.addEventListener('mouseover', animateText);
+    }
+
+    // Clean up the event listener
+    return () => {
+      if (heading) {
+        heading.removeEventListener('mouseover', animateText);
+      }
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="hero-container">
+      <div className="animated-text">
+        <h1 ref={headingRef} data-value="WELCOME TO SHANA'S PORTFOLIO">CLICK HERE</h1>
+      </div>
+
+
       <div className="left-image">
-        <img src={leftImage} alt='left-image'/>
-      
+        <img src={leftImage} alt="Left"/>
       </div>
 
       <div className="right-image">
-        
-        
+        <img src={rightImage} alt="Right"/>
       </div>
 
       <div className="scroll" style={{ '--t': '20s' }}>
@@ -25,6 +74,8 @@ function Hero() {
           <span>SHANA</span>
         </div>
       </div>
+
+
     </div>
   );
 }
--- port_v2/src/components/navbar.js
+++ port_v2/src/components/navbar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Navbar.css';
+import home from '/Users/shanakesia/Desktop/CMPT/projects/portfolio/port_v2/src/components/images/home_icon.png';
+import about from '/Users/shanakesia/Desktop/CMPT/projects/portfolio/port_v2/src/components/images/about_icon.png';
+import projects from '/Users/shanakesia/Desktop/CMPT/projects/portfolio/port_v2/src/components/images/projects_icon.png';
+import experience from '/Users/shanakesia/Desktop/CMPT/projects/portfolio/port_v2/src/components/images/experience_icon.png';
 
 function Navbar() {
   return (
@@ -7,13 +11,16 @@ function Navbar() {
       <div className="nav-container">
         <ul className="nav-menu">
           <li className="nav-item">
-            <a href="#hero" className="nav-links">Home</a>
+            <a href="#home" className="nav-links"><img src={home}/></a>
           </li>
           <li className="nav-item">
-            <a href="#projects" className="nav-links">Projects</a>
+            <a href="#about" className="nav-links"><img src={about}/></a>
           </li>
           <li className="nav-item">
-            <a href="#info" className="nav-links">Info</a>
+            <a href="#projects" className="nav-links"><img src={projects}/></a>
+          </li>
+          <li className="nav-item">
+            <a href="#experience" className="nav-links"><img src={experience}/></a>
           </li>
         </ul>
       </div>
